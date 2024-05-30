9d5903c61ba79b6a281c7386d35a84a54895aa25
hgi4657 <hgi4657@naver.com>
2024-05-30 21:09:12 +0900

    ✨ update : [댓글] 사용자 확인 코드 추가 - Comment 와 Schedule 연관관계 설정 - 댓글 등록 시 유저 ID 저장 - 댓글 수정, 삭제 시 등록했던 유저가 맞는지 확인

--- src/main/java/com/sparta/schedule/service/CommentService.java
+++ src/main/java/com/sparta/schedule/service/CommentService.java
@@ -4,10 +4,12 @@ import com.sparta.schedule.dto.CommentRequestDto;
 import com.sparta.schedule.dto.CommentResponseDto;
 import com.sparta.schedule.entity.Comment;
 import com.sparta.schedule.entity.Schedule;
+import com.sparta.schedule.entity.User;
 import com.sparta.schedule.repository.CommentRepository;
 import com.sparta.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,41 +18,51 @@ public class CommentService {
     private final ScheduleRepository scheduleRepository;
 
     // 댓글 등록
-    public CommentResponseDto addComment(Long scheduleId, CommentRequestDto commentRequestDto) {
+    public CommentResponseDto addComment(Long scheduleId, CommentRequestDto commentRequestDto, User user) {
         // 입력받은 일정 아이디가 DB에 존재하는지 확인
         // 댓글 내용이 공백 or " "이 아닌지 확인
         Schedule schedule = findScheduleById(scheduleId);
 
-        Comment comment = new Comment(commentRequestDto, schedule);
+        Comment comment = new Comment(commentRequestDto, schedule, user);
         commentRepository.save(comment);
 
         return new CommentResponseDto(comment);
     }
 
     // 댓글 수정
-    public CommentResponseDto updateComment(Long scheduleId, Long commentId, CommentRequestDto commentRequestDto) {
+    @Transactional
+    public CommentResponseDto updateComment(Long scheduleId, Long commentId, CommentRequestDto commentRequestDto, User user) throws IllegalAccessException {
         // 입력받은 일정 아이디가 DB에 존재하는지 확인
         // 입력받은 댓글 아이디가 DB에 존재하는지 확인
         // 입력받은 일정 아이디와 댓글 아이디가 제대로 맵핑된 것인지 확인
-
+        // 댓글을 입력한 유저와 로그인한 유저가 동일한지 확인
         Schedule schedule = findScheduleById(scheduleId);
         Comment comment = findCommentById(commentId);
 
         checkScheduleIdAndCommentId(schedule, comment);
+
+        Long loginUserId = user.getId();
+        Long commentUserId = comment.getUser().getId();
+        reqUserCheck(loginUserId, commentUserId);
 
         comment.update(commentRequestDto.getContent());
         return new CommentResponseDto(comment);
     }
 
     // 댓글 삭제
-    public Long deleteComment(Long scheduleId, Long commentId) {
+    public Long deleteComment(Long scheduleId, Long commentId, User user) throws IllegalAccessException {
         // 입력받은 일정 아이디가 DB에 존재하는지 확인
         // 입력받은 댓글 아이디가 DB에 존재하는지 확인
         // 입력받은 일정 아이디와 댓글 아이디가 제대로 맵핑된 것인지 확인
+        // 댓글을 입력한 유저와 로그인한 유저가 동일한지 확인
         Schedule schedule = findScheduleById(scheduleId);
         Comment comment = findCommentById(commentId);
 
         checkScheduleIdAndCommentId(schedule, comment);
+
+        Long loginUserId = user.getId();
+        Long commentUserId = comment.getUser().getId();
+        reqUserCheck(loginUserId, commentUserId);
 
         commentRepository.delete(comment);
         return commentId;
@@ -73,6 +85,13 @@ public class CommentService {
     private void checkScheduleIdAndCommentId(Schedule schedule, Comment comment) {
         if (!schedule.getId().equals(comment.getSchedule().getId())) {
             throw new IllegalArgumentException("해당 댓글은 선택하신 일정에 존재하지 않습니다.");
+        }
+    }
+
+    // 댓글을 입력한 유저와 로그인한 유저가 동일한지 확인
+    private void reqUserCheck(Long loginUserId, Long commentUserId) throws IllegalAccessException {
+        if (!loginUserId.equals(commentUserId)) {
+            throw new IllegalAccessException("해당 일정을 작성한 사용자가 아닙니다");
         }
     }
 }
